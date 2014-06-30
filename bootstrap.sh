e1935eb1c729dbe2932d875e53e7b1f98d710de1
Rafael Magrin <rafael.magrin@gmail.com>
2014-06-30 09:30:00 -0300

    Removing rubygems from required packages since it doesn't exist anymore.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ PROJECT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt-get install -y build-essential nodejs libsqlite3-dev
 
 sudo apt-get remove -y ruby1.9 ruby1.9.1
-sudo apt-get install -y ruby2.1 ruby2.1-dev rubygems
+sudo apt-get install -y ruby2.1 ruby2.1-dev
 
 sudo gem install bundle
 
