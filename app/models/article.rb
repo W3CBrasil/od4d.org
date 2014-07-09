c88bfa9ec4e325a5e2ffb98bf75d36dc7572781d
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-09 20:52:50 +0000

    Added link to share in social networks for articles page.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article
   attr_accessor :url, :title, :author, :summary, :description, :articleSection, :datePublished
+
+  def share_twitter
+     "https://twitter.com/intent/tweet?url=#{URI.escape(url)}&text=#{URI.escape(title)}&via=OD4D"
+  end
+
+  def share_linkedin
+     "http://www.linkedin.com/shareArticle?mini=true&url=#{URI.escape(url)}&title=#{URI.escape(title)}&source=#{URI.escape('OD4D.org')}"
+  end
+
+  def share_googleplus
+     "https://plusone.google.com/_/+1/confirm?hl=en&url=#{URI.escape(url)}"
+  end
 end
