6904b85419712ba03f7731a623f12cf7f3da1c30
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-09 17:07:58 +0000

    Added hack to try to fix prism.js issue with turbolinks.

--- app/assets/javascripts/prism-only.js
+++ app/assets/javascripts/prism-only.js
@@ -1,2 +1,8 @@
 //= require turbolinks
 //= require prism
+//= require_self
+
+// This is needed for prism to work with turbolinks
+$(document).on('ready page:load', function() {
+    Prism.highlightAll();
+});
