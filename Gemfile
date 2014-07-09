09ece9537c5fa5e5fb0e0b9b07fbd0e71dca37b8
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-09 17:57:45 +0000

    Removed turbolinks since its not bringing much benefits at this points and is causing some issues with prism.js

--- Gemfile
+++ Gemfile
@@ -1,11 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'jquery-rails'
 gem 'rails', '4.1.1'
 gem 'sass-rails', '~> 4.0.3'
 gem 'sqlite3'
 gem 'uglifier', '>= 1.3.0'
-gem 'turbolinks'
 
 group :development do
   gem 'capistrano-rails'
--- app/assets/javascripts/prism-only.js
+++ app/assets/javascripts/prism-only.js
@@ -1,13 +1,2 @@
-//= require jquery
 //= require prism
 //= require_self
-
-// This is needed for prism to work with turbolinks
-// reference: https://stackoverflow.com/questions/21278357/prism-js-not-working-with-rails-4-turbolinks
-console.log('0');
-//document.addEventListener("page:load", function() {
-$(document).on('ready page:load', function() {
-  console.log('1');
-  Prism.highlightAll();
-  console.log('2');
-});
