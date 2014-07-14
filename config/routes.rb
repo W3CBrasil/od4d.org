7d146e5ebb4e0a697ac896e188472d08d6bbfdc9
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-14 18:57:59 +0000

    Renaming old How to Create a Semantic Page to Introduction to RDF and RDFa.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'home#index'
+
   get '/about' => 'pages#about'
   get '/terms-and-conditions' => 'pages#terms_and_conditions'
-  get '/how-to-create-a-semantic-page' => 'pages#how_to_create_a_semantic_page'
+
   get '/how-to-integrate' => 'pages#how_to_integrate_with_od4d'
-  get '/how-to-new' => 'pages#how_to_new'
+  get '/introduction-to-rdf-and-rdfa' => 'pages#introduction_to_rdf_and_rdfa'
+  get '/how-to-create-a-semantic-page' => 'pages#how_to_create_a_semantic_page'
   get '/glossary' => 'pages#glossary'
+
   resources :articles, :only => [:index]
   resources :cases, :only => [:index]
   resources :partners, :only => [:index]
