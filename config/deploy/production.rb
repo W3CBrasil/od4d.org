de3b262858bed69ddc4a0e75637b3f5406f4ef73
Alfredo Segundo <alfredocavalcanti@gmail.com>
2014-09-15 19:49:05 -0300

    add db:migrate task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,5 @@ server_address = "#{ENV['OD4D_PROD_SERVER']}"
 raise "Please set the server address using the OD4D_PROD_SERVER environment variable".red if server_address.empty?
 
 set :stage, :production
+set :rails_env, 'production'
 server server_address, user: 'od4d', roles: %w{web app}
--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,2 +1,3 @@
 set :stage, :test
+set :rails_env, 'test'
 server "app-server.dev", user: 'od4d', roles: %w{web app}
