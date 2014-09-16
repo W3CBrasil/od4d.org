e1f50c3abb6e0cfbbf17074bc418c7d20cc82c38
Alfredo Segundo <alfredocavalcanti@gmail.com>
2014-09-16 11:08:45 -0300

    fix sintax error

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :user_name => "od4d.staging@gmail.com"
+    :user_name => "od4d.staging@gmail.com",
     :password => "9o9Amjw3GV",
     :authentication => "plain",
     :enable_starttls_auto => true
