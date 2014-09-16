58620b280c56aad67f06d2588c8c078ff03eb64f
Alfredo Segundo <alfredocavalcanti@gmail.com>
2014-09-16 11:18:09 -0300

    add smtp config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,15 @@ Rails.application.configure do
   config.use_google_analytics = false
 
   config.reload_classes_only_on_change = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "od4d.staging@gmail.com",
+    :password => "9o9Amjw3GV",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
