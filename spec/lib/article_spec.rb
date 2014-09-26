9720e0623257f3126ed88e1873a735e0e619fcbe
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-09-26 17:38:07 -0300

    W3CBrasil/OD4D#51 - pairing w/ @alfredosegundo - Filter/listing page

--- spec/lib/article_spec.rb
+++ spec/lib/article_spec.rb
@@ -7,7 +7,7 @@ describe Article do
 	context "#is_third_party?" do
 		before(:each) do
 			@article = Article.new()
-			@article.publisher = Partner.new URI("http://www.od4d.org")
+			@article.publisher = "http://platform.od4d.org"
 		end
 
 		it "should return FALSE if the partner is OD4D" do
@@ -15,7 +15,7 @@ describe Article do
 		end
 
 		it "should return TRUE if the partner isn't OD4D" do
-			@article.publisher.uri = "http://www.webfoundation.org"
+			@article.publisher = "http://www.webfoundation.org"
 			expect(@article.is_third_party?).to eq(true)
 		end
 
@@ -60,7 +60,7 @@ describe Article do
 				@article = Article.new()
 				@article.summary = "This is the article's summary"
 				@article.description = "This is the article's description"
-				@article.publisher = Partner.new URI("http://www.od4d.org")
+				@article.publisher = "http://platform.od4d.org"
 			end
 
 			it "should NOT truncate the article's content" do
